3dd69c44e3948eef9209dfc0d0898180f467598d
king-paul <PaulKing09@bigpond.com>
2023-07-11 16:56:07 +1000

    Added lanes to enemy test scene and updated mine attack

--- Assets/Scripts/Enemy/MineAttack.cs
+++ Assets/Scripts/Enemy/MineAttack.cs
@@ -6,15 +6,21 @@ public class MineAttack : EnemyAttack
 {
     public GameObject minePrefab;
 
-    [SerializeField] float minesToSpawn;
-    [SerializeField] float timeBetweenMines;
-    [SerializeField] Vector3[] spawnPoints;
+    [SerializeField] uint minesToSpawn = 10;
+    [SerializeField] float timeBetweenMines = 1;    
+    [SerializeField] bool selectSpawnPointsRandomly;
+    [SerializeField] bool dropMinesSimulatenously;
+    [SerializeField] Transform[] spawnPoints;
 
     float timeSinceLastMine = 0;
+    uint minesSpawned = 0;
+
+    private int spawnPointNumber = 0;
+    private bool descending = false;
 
     private void OnEnable()
     {
-        
+        DropMine();
     }
 
     // Update is called once per frame
@@ -25,6 +31,63 @@ public class MineAttack : EnemyAttack
         if(timeSinceLastMine >= timeBetweenMines)
         {
             timeSinceLastMine = 0;
+            DropMine();
+        }
+
+        if(minesSpawned >= minesToSpawn)
+        {
+            AttackFinished = true;
+            this.enabled = false;
+        }
+    }
+
+    private void DropMine()
+    {
+        if (dropMinesSimulatenously)
+        {
+            foreach (Transform spawnPoint in spawnPoints)
+            {
+                Instantiate(minePrefab, spawnPoint.position, Quaternion.identity);
+                minesSpawned++;
+            }
+        }
+        else if(selectSpawnPointsRandomly)
+        {
+            int randomPick;
+            do {
+                randomPick = Random.Range(0, 3);
+            } while (randomPick == 3);
+
+            Instantiate(minePrefab, spawnPoints[randomPick].position, Quaternion.identity);
+
+            minesSpawned++;
+        }
+        else
+        {
+            Instantiate(minePrefab, spawnPoints[spawnPointNumber].position, Quaternion.identity);
+
+            if (descending)
+            {
+                if (spawnPointNumber > 0)
+                    spawnPointNumber--;
+                else
+                {
+                    spawnPointNumber++;
+                    descending = false;
+                }
+            }
+            else
+            {
+                if (spawnPointNumber < 2)
+                    spawnPointNumber++;
+                else
+                {
+                    spawnPointNumber--;
+                    descending = true;
+                }
+            }
+
+            minesSpawned++;
         }
     }
 }
